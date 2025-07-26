608a5d743e92bdfdd60852fe14efb1ea7600bf5a
Ansh <ansh.madan_ug25@ashoka.edu.in>
2025-07-26 13:03:07 +0530

    Made changes to models for scraped items

--- scrapy_webscraper/scrapy_webscraper/items.py
+++ scrapy_webscraper/scrapy_webscraper/items.py
@@ -1,16 +1,12 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
+# Models for the scraped items are defined here.
 # https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
-
 
 class ProductItem(scrapy.Item):
     name = scrapy.Field()
     price = scrapy.Field()
     url = scrapy.Field()
     product_id = scrapy.Field()
-    image_url = scrapy.Field()          # Main/first image (for backward compatibility)
-    image_urls = scrapy.Field()         # List of all image URLs
-    image_count = scrapy.Field()        # Number of images (optional)
+    image_url = scrapy.Field()
+    image_urls = scrapy.Field()
+    image_count = scrapy.Field()
